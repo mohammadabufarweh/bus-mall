e26d056146687bee105a27c97e4fdcb184385c6e
Mohammadabufarweh <abufarweh34@gmail.com>
2021-04-26 13:47:53 +0300

    final-lab11

--- js/app.js
+++ js/app.js
@@ -19,9 +19,8 @@ let imgArray = [
   'water-can.jpg',
   'wine-glass.jpg'
 ];
-
+const viewResult = document.getElementById('click');
 const parentElement = document.getElementById('submitt');
-//const imageSection = document.getElementById('imageSection');
 const leftImage = document.getElementById('leftImage');
 const rightImage = document.getElementById('rightImage');
 const midleImage = document.getElementById('midleImage');
@@ -32,25 +31,20 @@ let midleClick = 0;
 
 
 function Bus(name) {
-  this.name = name;
+  this.name = name.split('.')[0];
+
   this.img = `./img/${name}`;
   this.shown = 0;
   this.clicks = 0;
   Bus.all.push(this);
 }
-function Ss(name) {
-  this.name = name;
-  this.shownn = 0;
-  this.clickss = 0;
-  Ss.all.push(this);
-}
 
-Ss.all=[];
+
 Bus.all = [];
 
 for (let i = 0; i < imgArray.length; i++) {
   new Bus(imgArray[i]);
-  new Ss (imgArray[i]);
+ 
 }
 
 function eventHandler(e) {
@@ -59,31 +53,36 @@ function eventHandler(e) {
 
     if (e.target.id == 'leftImage') {
       Bus.all[leftClick].clicks++;
-      Ss.all[leftClick].clickss++;
+     
     }
     if (e.target.id == 'rightImage') {
       Bus.all[rightClick].clicks++;
-      Ss.all[rightClick].clickss++;
+     
     }
     if (e.target.id == 'midleImage') {
       Bus.all[midleClick].clicks++;
-      Ss.all[midleClick].clickss++;
+     
     }
     clickCounter++;
     renderContent();
   }
   else {
     console.log(Bus.all);
-    console.log(Ss.all);
+   
     
   }
 }
 
 function renderContent() {
   let leftPart = theRandomNumber(0, imgArray.length - 1);
-  let midlePart = theRandomNumber(0, imgArray.length - 1);
-  let rightPart = theRandomNumber(0, imgArray.length - 1);
-  
+  let midlePart ; 
+  let rightPart ;
+
+  do {
+  midlePart = theRandomNumber(0, imgArray.length - 1);
+  rightPart = theRandomNumber(0, imgArray.length - 1);
+  }while(midlePart===rightPart || midlePart===leftPart || leftPart===rightPart )
+
   leftImage.src = Bus.all[leftPart].img;
   rightImage.src = Bus.all[rightPart].img;
   midleImage.src = Bus.all[midlePart].img;
@@ -94,35 +93,29 @@ function renderContent() {
   Bus.all[rightPart].shown++;
   Bus.all[leftPart].shown++;
   
-  Ss.all[midlePart].shownn++;
-  Ss.all[rightPart].shownn++;
-  Ss.all[leftPart].shownn++;
+
   
   // console.log(Bus.all);
-}
-
-function renderContenttt() {
-  let articleElement = document.createElement('article');
-  parentElement.appendChild(articleElement);
-  
-  const h2Element = document.createElement('h2');
-  h2Element.textContent = Ss.all.target;
-  articleElement.appendChild(h2Element);
-}
-
+} 
 
 function eventButton(event) {
   event.preventDefault();
-  renderContenttt();
+   let ulElement = document.createElement('ul');
+  parentElement.appendChild(ulElement);
+  for (let i=0;i<Bus.all.length;i++){
+  const liElement = document.createElement('li');
+  ulElement.appendChild(liElement)
+  liElement.textContent = `${Bus.all[i].name} had a ${Bus.all[i].clicks}  votes,and was seen  ${Bus.all[i].shown}`;
+  viewResult.removeEventListener('click',eventButton);
 }
-
+}
 function theRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
-renderContenttt();
+
 
 renderContent();
 imageSection.addEventListener('click', eventHandler);
-submitt.addEventListener('submit', eventButton)
+viewResult.addEventListener('click', eventButton);
